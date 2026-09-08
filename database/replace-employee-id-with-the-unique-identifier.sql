d4ccd7b32f88a9545af03d4fd7f3f6e0ca553ac2
Himanshu Pandey <hp184020@gmail.com>
2026-09-09 01:30:09 +0530

    LC-replace-employee-id-with-the-unique-identifier [Runtime: 1241 ms; Memory: 0B]

---
+++ database/replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select unique_id,name from Employees left join EmployeeUNI on Employees.id=EmployeeUNI.id; 
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
