fd88a3a4901a62343d4d82ee0361043c7bb825b4
Himanshu Pandey <hp184020@gmail.com>
2026-09-09 11:28:20 +0530

    LC-product-sales-analysis-i [Runtime: N/A; Memory: N/A]

---
+++ database/product-sales-analysis-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT product_name,year,price from Sales inner join Product on Sales.product_id=Product.product_id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
