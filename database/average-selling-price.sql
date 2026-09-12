f6fa342c39ca41c84463f5c2a8a47f57249065f1
Himanshu Pandey <hp184020@gmail.com>
2026-09-12 15:27:51 +0530

    LC-average-selling-price [Runtime: N/A; Memory: N/A]

---
+++ database/average-selling-price.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select p.product_id,round(sum(p.price*u.units)/sum(u.units),2) as average_price from Prices p left join UnitsSold u on p.product_id=u.product_id and u.purchase_date between p.start_date and p.end_date group by p.product_id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
