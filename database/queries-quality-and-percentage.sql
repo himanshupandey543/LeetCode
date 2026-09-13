fb77da26487f97768cac19992ba9da6cd3eabf37
Himanshu Pandey <hp184020@gmail.com>
2026-09-13 11:26:58 +0530

    LC-queries-quality-and-percentage [Runtime: N/A; Memory: N/A]

---
+++ database/queries-quality-and-percentage.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select query_name,round(avg(rating/position),2) as quality,round(100*sum(case when rating<3 then 1 else 0 end)/count(*),2) as poor_query_percentage from Queries group by query_name;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
