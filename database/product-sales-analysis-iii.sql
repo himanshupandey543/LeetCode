29f960b5e78de977f8dd2dc7b08f9286562884b0
Himanshu Pandey <hp184020@gmail.com>
2026-09-14 15:33:34 +0530

    LC-product-sales-analysis-iii [Runtime: N/A; Memory: N/A]

--- database/product-sales-analysis-iii.sql
+++ database/product-sales-analysis-iii.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select product_id,year as first_year,quantity,price from Sales group by product_id;
+select product_id,year as first_year,quantity,price from Sales where(product_id,year) in (select product_id,min(year) from sales group by product_id);
 
 -- Synced seamlessly with LeetHub Pro
 -- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
