fa0a5cd44251f4ead3e16bee05eef5798d58d8cc
Himanshu Pandey <hp184020@gmail.com>
2026-09-14 14:33:59 +0530

    LC-product-sales-analysis-iii [Runtime: N/A; Memory: N/A]

---
+++ database/product-sales-analysis-iii.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select product_id,year as first_year,quantity,price from Sales group by product_id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
