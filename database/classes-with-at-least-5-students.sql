9e54a77c9143242acbc6d191ff7645e5b53298f1
Himanshu Pandey <hp184020@gmail.com>
2026-09-14 15:59:35 +0530

    LC-classes-with-at-least-5-students [Runtime: N/A; Memory: N/A]

---
+++ database/classes-with-at-least-5-students.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select class from Courses group by class having count(student)>=5;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
