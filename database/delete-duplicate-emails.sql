f41f55c9552c64edf0c238ddea5a653c956ea321
Himanshu Pandey <hp184020@gmail.com>
2026-09-19 13:56:09 +0530

    LC-delete-duplicate-emails [Runtime: N/A; Memory: N/A]

---
+++ database/delete-duplicate-emails.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+delete p1 from Person p1,Person p2 where p1.email=p2.email and p1.id>p2.id; 
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
