d468c5074a94ae5fd62964a6d2d43be1558d4f08
Himanshu Pandey <hp184020@gmail.com>
2026-09-20 09:03:28 +0530

    LC-patients-with-a-condition [Runtime: N/A; Memory: N/A]

--- database/patients-with-a-condition.sql
+++ database/patients-with-a-condition.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select * from Patients where conditions like '%DIAB1%';
+select * from Patients where conditions like '%DIAB1%' group by conditions;
 
 -- Synced seamlessly with LeetHub Pro
 -- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
