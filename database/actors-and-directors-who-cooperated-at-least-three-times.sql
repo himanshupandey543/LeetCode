cb8dac07380f1c72b65ef5ec07b35db77d1ede63
Himanshu Pandey <hp184020@gmail.com>
2026-09-23 21:18:38 +0530

    LC-actors-and-directors-who-cooperated-at-least-three-times [Runtime: 417 ms; Memory: 0B]

---
+++ database/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select actor_id,director_id from ActorDirector group by actor_id,director_id having Count(actor_id=director_id)>=3;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
