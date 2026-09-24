ad3e9749c36f66c721c441a3cfc56cb5113fc02a
Himanshu Pandey <hp184020@gmail.com>
2026-09-24 23:31:26 +0530

    LC-customer-placing-the-largest-number-of-orders [Runtime: 463 ms; Memory: 0B]

---
+++ database/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select customer_number from Orders group by customer_number order by count(customer_number) desc limit 1;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
